223cfe73d13deed77652e1688c36942da51e4f3f
Romain Thouvenin <romain@wemove.eu>
2016-05-24 15:03:40 +0200

    Toggle other amount and align recurring input

--- js/contribjs.js
+++ js/contribjs.js
@@ -1,1 +1,11 @@
-console.log('eu.wemove.contribjs');
+jQuery('.crm-section.is_recur-section > .label').remove();
+
+jQuery('.price-set-row input').on('change', function(e) {
+  var $other = jQuery('.crm-container .crm-section.other_amount-section'); 
+  var val = jQuery(this).val();
+  if (val == 0) {
+    $other.show();
+  } else {
+    $other.hide();
+  }
+});
