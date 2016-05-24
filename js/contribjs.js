0472c7d00ac113a3c01ff364ef2ebbddbb5d229d
Romain Thouvenin <romain@wemove.eu>
2016-05-24 19:00:55 +0200

    Prepare for multiple countries

--- js/contribjs.js
+++ js/contribjs.js
@@ -27,35 +27,54 @@ function addField(name, label) {
   $iban.before(field);
 }
 
-function mod97(digits) {
-  var value = 0;
-  for (var i = 0; i < digits.length; ++i)
-    value = (10 * value + parseInt(digits.charAt(i))) % 97;
+function replaceLetters(bban) {
+  var value = '';
+  var a = 'a'.charCodeAt(0) - 10;
+  bban = bban.toLowerCase();
+  for (var i = 0; i < bban.length; ++i) {
+    var c = bban.charAt(i);
+    var n = parseInt(c);
+    if (isNaN(n)) n = bban.charCodeAt(i) - a;
+    value = value + n;
+  }
   return value;
 }
 
-function genIBAN() {
+function mod97(digits) {
+  var value = 0;
+  for (var i = 0; i < digits.length; ++i) {
+    value = (10 * value + parseInt(digits.charAt(i))) % 97;
+  }
+  return value;
+}
+
+function getBBAN(country) {
   var bank_code = jQuery('#bank_code').val(),
       branch_code = jQuery('#branch_code').val(),
       check_digits = jQuery('#check_digits').val(),
       account = jQuery('#account').val();
 
-  var bban = '' + bank_code + branch_code + check_digits + account;
-  var iban_prefix = '181500';
-  var checksum = (98 - mod97(bban + iban_prefix)).toString();
+  return '' + bank_code + branch_code + check_digits + account;
+}
+
+function genIBAN(country) {
+  var bban = getBBAN(country);
+  var checkInput = replaceLetters(bban + country + '00');
+  var checksum = (98 - mod97(checkInput)).toString();
   while (checksum.length < 2) checksum = '0' + checksum;
-  return 'SP' + checksum + bban;
+  return country + checksum + bban;
 }
 
 var formId = getParam('id');
 if (isIBANConverted(formId)) {
+  var country = 'ES';
   var $iban = jQuery('#bank_account_number');
   addField('bank_code', "Bank code");
   addField('branch_code', "Branch code");
   addField('check_digits', "Check digits");
   addField('account', "Account number");
   jQuery('#bank_code, #branch_code, #check_digits, #account').on('change', function(e) {
-    $iban.val(genIBAN());
+    $iban.val(genIBAN(country));
   });
 }
 
