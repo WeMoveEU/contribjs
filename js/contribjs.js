ace3cb0ff0351d2c163075a1bb1fe3dbb66dc04e
Romain Thouvenin <romain@wemove.eu>
2016-06-02 14:52:21 +0200

    execute DOM code after everything else

--- js/contribjs.js
+++ js/contribjs.js
@@ -1,29 +1,7 @@
-// Set by earlier in a drupal block
-var contribConfig = window.contribConfig || {};
 
 function getParam(name) {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 }
-
-// set default amount based on param donation_amount in url
-var name = 'donation_amount';
-var da = getParam(name);
-if (da > 0) {
-  da = da.replace('.', '\\.');
-  jQuery(".price-set-row input[data-amount="+da+"]").prop("checked", true).click();
-}
-
-
-//Show / hide other amount input
-jQuery('.price-set-row input').on('change', function(e) {
-  var $other = jQuery('.crm-container .crm-section.other_amount-section'); 
-  var val = jQuery(this).val();
-  if (val == 0) {
-    $other.show();
-  } else {
-    $other.hide();
-  }
-});
 
 
 /* Convert daily/weekly donations to monthly donations */
@@ -54,27 +32,6 @@ function updateMonthlyValue() {
   jQuery('#monthlyValue').text(monthlyValue);
 }
 
-if (isConvertedToMonthly()) {
-  var $monthlyInfo = jQuery('<div class="crm-section"><div class="label"></div><div class="content" id="monthlyInfo">You will contribute € <span id="monthlyValue"></span> per month</div><div class="clear"></div></div>');
-  jQuery('.other_amount-section').after($monthlyInfo);
-  jQuery('.price-set-row input').on('change', function(e) {
-    updateMonthlyValue();
-  });
-  jQuery('.other_amount-section input').on('keyup', function(e) {
-    updateMonthlyValue();
-  });
-  updateMonthlyValue();
-
-  jQuery('form#Main').on('submit', function(e) {
-    jQuery('.other_amount-section input').val(getMonthlyValue());
-    jQuery('.price-set-row input[value=0]').prop('checked', 'checked');
-  });
-}
-
-/* Converting comma to dot in other amount field */
-jQuery(".other_amount-content input[name*=price]").change(function(e) {
-  jQuery(this).val(jQuery(this).val().replace(/,/g,"."));
-});
 
 
 /* IBAN magic */
@@ -164,16 +121,61 @@ function enableNationalForm(country) {
   });
 }
 
-if (isIBANConverted(contribConfig.pageId)) {
-  var country = 'DE';
-  var $payProc = jQuery('input[name=payment_processor]');
-  if (jQuery('.direct_debit_info-group').length) {
-    enableNationalForm(country);
+jQuery(function($) {
+  // Set by earlier in a drupal block
+  var contribConfig = window.contribConfig || {};
+
+  // set default amount based on param donation_amount in url
+  var name = 'donation_amount';
+  var da = getParam(name);
+  if (da > 0) {
+    da = da.replace('.', '\\.');
+    jQuery(".price-set-row input[data-amount="+da+"]").prop("checked", true).click();
   }
-  $payProc.on('change', function(e) {
-    if ($payProc.filter(':checked').val() == '3') {
-      enableNationalForm(country);
+
+
+  //Show / hide other amount input
+  jQuery('.price-set-row input').on('change', function(e) {
+    var $other = jQuery('.crm-container .crm-section.other_amount-section'); 
+    var val = jQuery(this).val();
+    if (val == 0) {
+      $other.show();
+    } else {
+      $other.hide();
     }
   });
-}
 
+  if (isConvertedToMonthly()) {
+    var $monthlyInfo = jQuery('<div class="crm-section"><div class="label"></div><div class="content" id="monthlyInfo">You will contribute € <span id="monthlyValue"></span> per month</div><div class="clear"></div></div>');
+    jQuery('.other_amount-section').after($monthlyInfo);
+    jQuery('.price-set-row input').on('change', function(e) {
+      updateMonthlyValue();
+    });
+    jQuery('.other_amount-section input').on('keyup', function(e) {
+      updateMonthlyValue();
+    });
+    updateMonthlyValue();
+
+    jQuery('form#Main').on('submit', function(e) {
+      jQuery('.other_amount-section input').val(getMonthlyValue());
+      jQuery('.price-set-row input[value=0]').prop('checked', 'checked');
+    });
+  }
+
+  /* Converting comma to dot in other amount field */
+  jQuery(".other_amount-content input[name*=price]").change(function(e) {
+    jQuery(this).val(jQuery(this).val().replace(/,/g,"."));
+  });
+  if (isIBANConverted(contribConfig.pageId)) {
+    var country = 'DE';
+    var $payProc = jQuery('input[name=payment_processor]');
+    if (jQuery('.direct_debit_info-group').length) {
+      enableNationalForm(country);
+    }
+    $payProc.on('change', function(e) {
+      if ($payProc.filter(':checked').val() == '3') {
+	enableNationalForm(country);
+      }
+    });
+  }
+});
